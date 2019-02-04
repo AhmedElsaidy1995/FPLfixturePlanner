777be568d93aaf61647f4d8d52c0250ae72804e8
Ahmed Elsaidy <ahmedelsaidy1995@gmail.com>
2019-02-04 21:53:26 +0200

    intentAdded

--- app/src/main/java/com/example/sasadoe/fplfixtureplanner/MainActivity.kt
+++ app/src/main/java/com/example/sasadoe/fplfixtureplanner/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.sasadoe.fplfixtureplanner
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shirtImages = arrayListOf(player1img,player2img,player3img,player4img,player5img,player6img,player7img,player8img,player9img,player10img,player11img,player12img,player13img,player14img,player15img)
+
+        for (shirt in shirtImages) {
+            shirt.setOnClickListener{
+                val editPlayerIntent = Intent(this,EditActivity::class.java)
+                startActivity(editPlayerIntent)
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
